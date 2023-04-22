abc482c9deba637a7ec9941be3b28d3f342b6159
Mustafa Iraqi <118061209+Telegram-R2RR7@users.noreply.github.com>
2023-04-23 01:12:40 +0300

    Add files via upload

--- X2RZ7WX/core/git.py
+++ X2RZ7WX/core/git.py
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TeamYukki/X2RZ7WXBot > project,
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/X2RZ7WXBot/blob/master/LICENSE >
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -62,3 +62,4 @@ def git():
         else:
             origin = repo.create_remote("origin", UPSTREAM_REPO)
         origin.fetch()
+        
